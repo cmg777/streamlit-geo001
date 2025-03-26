2034e0c9a4c693d8d566cd863b85effead4d21a9
Carlos Mendez <carlosmendez777@gmail.com>
2025-03-26 10:11:11 +0900

    dfdf

--- Home.py
+++ Home.py
@@ -3,43 +3,38 @@ import leafmap.foliumap as leafmap
 
 st.set_page_config(layout="wide")
 
-# Customize the sidebar
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
-
+# Sidebar configuration
 st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+st.sidebar.info(
+    """
+    A Streamlit map template  
+    [GitHub Repository](https://github.com/opengeos/streamlit-map-template)
+    """
+)
+st.sidebar.image("https://i.imgur.com/UbOXYAU.png")
 
-# Customize page title
+# Main page title and description
 st.title("Streamlit for Geospatial Applications")
-
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    This multipage app template showcases interactive web apps built with [Streamlit](https://streamlit.io) and [leafmap](https://leafmap.org).  
+    Contributions are welcome on the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
     """
 )
 
+# Instructions with bullet points
 st.header("Instructions")
+st.markdown(
+    """
+    - 🚀 **Template Usage:** Use the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or generate your own template [here](https://github.com/opengeos/streamlit-map-template/generate).  
+    - 🎨 **Customize Sidebar:** Edit the sidebar text and logo in the Python files.  
+    - 🔍 **Find Emojis:** Visit [Emojipedia](https://emojipedia.org) for more options.  
+    - 📂 **Add New Apps:** Place new apps in the `pages/` directory with an emoji in the filename, e.g., `1_🚀_Chart.py`.  
+    - 🔗 **Additional Resources:** Explore the [GitHub repository](https://github.com/cmg777/streamlit-geo001/) and the [Streamlit app](https://quarcs-geo001.streamlit.app/).
+    """
+)
 
-markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Findemojis from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
-1. For the [GitHub repository](https://github.com/cmg777/streamlit-geo001/) 
-2. For the [Steamlit app](https://quarcs-geo001.streamlit.app/)
-
-
-"""
-
-
-
-st.markdown(markdown)
-
+# Interactive map with minimap control
 m = leafmap.Map(minimap_control=True)
 m.add_basemap("NASAGIBS.ViirsEarthAtNight2012")
 m.to_streamlit(height=500)
